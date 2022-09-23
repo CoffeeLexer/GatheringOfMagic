b2645b87330ec4b96bf3a553aee01742ef2d6d49
T4rt4ru5 <vitkuseduardas@gmail.com>
2022-09-23 14:28:53 +0300

    gitignore update

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const express = require('express')
 const app = express();
-const cookie_parser = require('cookie-parser')
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookie_parser)
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
