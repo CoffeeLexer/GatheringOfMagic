4d5d379f35571fec47409853f837f1c1aa880140
T4rt4ru5 <vitkuseduardas@gmail.com>
2022-09-23 14:31:03 +0300

    gitignore update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const app = express();
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('Hello World!')
 })
 
-app.listen(80, () => {
-    console.log('Running')
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
